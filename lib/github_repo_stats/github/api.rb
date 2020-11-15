62a42d93360d6d363950ce66fe2fd5b134eb0e78
HeRoMo <hero@asterisk-works.jp>
2020-11-15 19:22:03 +0900

    improvement

--- lib/github_repo_stats/github/api.rb
+++ lib/github_repo_stats/github/api.rb
@@ -1,19 +1,18 @@
 # frozen_string_literal: true
 
-# 参考
-# https://qiita.com/skuroki@github/items/eecc454edb2ac984be25
-# https://developer.github.com/v4/explorer/
-# https://github.com/github/graphql-client
-# https://medium.com/studist-dev/github-pr-analysis-d7cc51e76973
 require 'graphql/client'
 require 'graphql/client/http'
 
 module GithubRepoStats
   module Github
     module Api
-      HTTP = GraphQL::Client::HTTP.new('https://api.github.com/graphql') do
+      GITHUB_GRAPHQL_ENDPOINT = 'https://api.github.com/graphql'
+      HTTP = GraphQL::Client::HTTP.new(GITHUB_GRAPHQL_ENDPOINT) do
         def headers(context)
-          { 'Authorization' => "Bearer #{ENV['ACCESS_TOKEN']}" }
+          token = context[:access_token] || ENV['GITHUB_ACCESS_TOKEN']
+          raise 'Missing GitHub access token' unless token
+
+          { 'Authorization' => "Bearer #{token}" }
         end
       end
       Schema = GraphQL::Client.load_schema(HTTP)
