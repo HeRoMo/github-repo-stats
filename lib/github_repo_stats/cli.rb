64174be643b009fa54dd0e0351dfb01ebb147043
HeRoMo <hero@asterisk-works.jp>
2020-11-24 19:17:59 +0900

    fix bug

--- lib/github_repo_stats/cli.rb
+++ lib/github_repo_stats/cli.rb
@@ -9,17 +9,21 @@ module GithubRepoStats
   #
   class CLI < Thor
     desc 'stats', 'aggregate reporsitory stats'
-    method_option :repo, type: :string, aliases: '-r', require: true, desc: "repository's owner/repo"
-    method_option :term, type: :string, aliases: '-t', require: true, desc: 'term of aggregate [yyyy-mm-dd..yyyy-mm-dd]'
+    method_option :repo, type: :string, aliases: '-r', required: true, desc: "repository's owner/repo"
+    method_option :term, type: :string, aliases: '-t', required: true, desc: 'term of aggregate [yyyy-mm-dd..yyyy-mm-dd]'
     def stats
       repo = options[:repo]
       term = options[:term]
       client = GithubRepoStats::Client.new
-      result = client.exec(repo, term)
+      result = client.stats_of_repo(repo, term)
       pp result
     rescue StandardError => e
       warn e.message
       warn e.backtrace
+    end
+
+    def self.exit_on_failure?
+      true
     end
   end
 end
