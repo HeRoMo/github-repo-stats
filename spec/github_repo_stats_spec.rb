20648317ec032e8e27ba16d07ee2c12465af7bcc
HeRoMo <hero@asterisk-works.jp>
2020-12-30 18:36:58 +0900

    remove unnecessarily test

--- spec/github_repo_stats_spec.rb
+++ spec/github_repo_stats_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe GithubRepoStats do
   it 'has a version number' do
     expect(GithubRepoStats::VERSION).not_to be_nil
   end
-
-  xit 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
