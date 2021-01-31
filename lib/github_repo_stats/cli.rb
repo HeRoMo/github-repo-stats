14d0c71ffb6e49bcc85e71ce63ca79ff7e2447b9
HeRoMo <hero@asterisk-works.jp>
2021-01-31 11:54:54 +0900

    add vervose option to repo subcommand

--- lib/github_repo_stats/cli.rb
+++ lib/github_repo_stats/cli.rb
@@ -25,6 +25,9 @@ module GithubRepoStats
 
       client = GithubRepoStats::Client.new
       result = client.pulls_of_repo(repo, start_month, end_month)
+      if (options[:verbose] != true)
+        result.delete(:pull_requests)
+      end
       puts JSON.pretty_generate(result)
     rescue StandardError => e
       warn e.message
